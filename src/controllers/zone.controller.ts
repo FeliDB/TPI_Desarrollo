d3999dbca01ee8090ee27208f67555cefe3b94b2
Felipe Diaz Bertero <felipediazb16@gmail.com>
2025-05-10 15:19:20 -0300

    Componente de zonas funcional

--- src/controllers/zone.controller.ts
+++ src/controllers/zone.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post, Get } from '@nestjs/common';
+import { Body, Controller, Post, Get, Put, Param, Patch, Delete } from '@nestjs/common';
 import { ZoneService } from '../services/zone.service';
+import { identity } from 'rxjs';
 
 @Controller('zone')
 export class ZoneController {
@@ -8,6 +9,26 @@ export class ZoneController {
     @Post()
     postZonaEntrega(@Body() body: any){
         return this.zoneService.postZonaEntrega(body);
+    }
+
+    @Get()
+    getZonaEntrega(){
+        return this.zoneService.getZonaEntrega();
+    }
+
+    @Put(':id')
+    putZonaEntrega(@Param('id') id: number, @Body() body: any){
+        return this.zoneService.putZonaEntrega(id, body);
+    }
+
+    @Patch(':id')
+    patchZonaEntrega(@Param('id') id: number, @Body() body: any){
+        return this.zoneService.patchZonaEntrega(id, body);
+    }
+
+    @Delete(':id')
+    deleteZonaEntrega(@Param('id') id: number){
+        return this.zoneService.deleteZonaEntrega(id);
     }
 
 }
