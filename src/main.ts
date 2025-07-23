2c289848ff29b0cbf42cdfd588c5765405f9d55e
Felipe <felipediazb16@gmail.com>
2025-07-23 16:40:04 -0300

    Proyecto terminado

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-  await app.listen(process.env.PORT ?? 3001);
-=======
-  app.enableCors()
-  await app.listen(process.env.PORT ?? 3000);
-  // const corsOptions: CorsOptions = {
-  //   origin: '*', // Permitir solicitudes desde cualquier origen
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos HTTP permitidos
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  // };
->>>>>>> 78012b475187c1971991e658e364085fb77dbe74
+
+  // acá habilitamos CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // permite solo tu frontend Angular
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
